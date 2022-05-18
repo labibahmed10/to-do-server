8635327f0d0ec0e5992f02f935e01424b0815754
labibahmed10 <labib.ahmed.372@gmail.com>
2022-05-19 02:54:28 +0600

    added get method for all to do,post to upload to do's

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.TO_DO_USER}:${process.env.TO_DO_PASS}@cluster0.zqp7w.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -20,6 +20,24 @@ async function run() {
   try {
     await client.connect();
     const todoCollection = client.db("to-do").collection("allworks");
+
+    app.get("/allworks", async (req, res) => {
+      const result = await todoCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.post("/allworks", async (req, res) => {
+      const todoInfo = req.body;
+      const result = await todoCollection.insertOne(todoInfo);
+      res.send(result);
+    });
+
+    app.delete("allworks/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await todoCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     console.log("Connected to DB");
   }
